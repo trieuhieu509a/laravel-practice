ced20e03eb57cb85f6cefe2700c420cac663704a
Hieu Trinh <hieu.trinh@bliksund.com>
2022-07-04 17:08:45 +0700

    25 Constraining Posible Route Paramaters Values

--- routes/web.php
+++ routes/web.php
@@ -23,10 +23,14 @@ Route::get('/contact', function () {
 
 Route::get('/posts/{id}', function ($id) {
     return 'Blog post ' . $id;
-})->name('posts.show');
+})
+    // ->where([
+    //     'id' => '[0-9]+'
+    // ])
+    ->name('posts.show');
 
 Route::get('/recent-posts/{days_ago?}', function ($daysAgo = 20) {
     return 'Posts from ' . $daysAgo . ' days ago';
-})->name('posts.recent.index');
+});
 
 
