7fb1e02d63b6620a07b564c75afa1aab48e42355
Hieu Trinh <hieu.trinh@bliksund.com>
2022-07-12 09:28:48 +0700

    90 Testing update() action ( models update )

--- database/migrations/2022_07_07_101413_create_blog_posts_table.php
+++ database/migrations/2022_07_07_101413_create_blog_posts_table.php
@@ -15,10 +15,9 @@ class CreateBlogPostsTable extends Migration
     {
         Schema::create('blog_posts', function (Blueprint $table) {
             $table->id();
-            $table->timestamps();
-
             $table->string('title')->default('');
             $table->text('content')->default('');
+            $table->timestamps();
         });
     }
 
