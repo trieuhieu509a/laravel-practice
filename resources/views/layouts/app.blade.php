0f49f8be4cfb168315e9b56a70a7bd91a1538b71
Hieu Trinh <hieu.trinh@bliksund.com>
2022-07-21 10:55:23 +0700

    115 Logging out, @guest directive, debugging, CSRF token errors #0058

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,6 +18,21 @@
         <a class="p-2 text-dark" href="{{ route('home.contact') }}">Contact</a>
         <a class="p-2 text-dark" href="{{ route('posts.index') }}">Blog Posts</a>
         <a class="p-2 text-dark" href="{{ route('posts.create') }}">Add Blog Post</a>
+        @guest
+            @if (Route::has('register'))
+                <a class="p-2 text-dark" href="{{ route('register') }}">Register</a>
+            @endif
+            <a class="p-2 text-dark" href="{{ route('login') }}">Login</a>
+        @else
+            <a class="p-2 text-dark" href="{{ route('logout') }}"
+               onclick="event.preventDefault();document.getElementById('logout-form').submit();"
+            >Logout</a>
+
+            <form id="logout-form" action={{ route('logout') }} method="POST"
+                  style="display: none;">
+                @csrf
+            </form>
+        @endguest
     </nav>
 </div>
 <div class="container">
