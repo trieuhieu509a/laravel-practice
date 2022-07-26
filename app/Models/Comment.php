6c6f61d0be82e6a2a7cad848f374e2fe5cca3987
Hieu Trinh <hieu.trinh@bliksund.com>
2022-07-26 18:08:09 +0700

    144 Global Query Scopes and potential issues ( #0087 )

--- app/Models/Comment.php
+++ app/Models/Comment.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use App\Scopes\LatestScope;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\SoftDeletes;
@@ -17,5 +18,13 @@ class Comment extends Model
         // return $this->belongsTo('App\BlogPost', 'post_id', 'blog_post_id');
         return $this->belongsTo('App\Models\BlogPost');
 
+    }
+
+
+    public static function boot()
+    {
+        parent::boot();
+
+        static::addGlobalScope(new LatestScope);
     }
 }
