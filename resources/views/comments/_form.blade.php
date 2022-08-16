fd01ef1c002368fc65d00913aa027c7b70c6881d
Hieu Trinh <hieu.trinh@bliksund.com>
2022-08-16 13:16:11 +0700

    178 Route Model Binding #121

--- resources/views/comments/_form.blade.php
+++ resources/views/comments/_form.blade.php
@@ -9,6 +9,7 @@
 
             <button type="submit" class="btn btn-primary btn-block">Add comment</button>
         </form>
+        <x-errors></x-errors>
     @else
         <a href="{{ route('login') }}">Sign-in</a> to post comments!
     @endauth
