506dd86e6e1b99484a1aa1845dcf06d939771968
Hieu Trinh <hieu.trinh@bliksund.com>
2022-09-06 17:56:34 +0700

    203. Practical: Running tests on MySQL database #146

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -37,10 +37,14 @@ class PostTest extends TestCase
 
     public function testSee1BlogPostWithComments()
     {
+        $user = $this->user();
+
         // Arrange
         $post = $this->createDummyBlogPost();
         Comment::factory(4)->create([
-            'blog_post_id' => $post->id
+            'commentable_id' => $post->id,
+            'commentable_type' => 'App\Models\BlogPost',
+            'user_id' => $user->id
         ]);
 
         $response = $this->get('/posts');
