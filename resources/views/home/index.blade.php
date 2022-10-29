d52fdd11e4930f3054a96d64b3c5212856d67e81
Hieu Trinh <hieu.trinh@bliksund.com>
2022-10-29 22:00:03 +0700

    235. Translating plural forms, passing data #178

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -1,24 +1,14 @@
 @extends('layouts.app')
 
-@section('title', 'Home page')
-
 @section('content')
-    <h1>Hello world!</h1>
+    <h1>{{ __('messages.welcome') }}</h1>
+    <h1>@lang('messages.welcome')</h1>
 
-    <div>
-        @for ($i = 0; $i < 10; $i++)
-            <div>The current value is {{ $i }}</div>
-        @endfor
-    </div>
+    <p>{{ __('messages.example_with_value', ['name' => 'John']) }}</p>
 
-    <div>
-        @php $done = false @endphp
-        @while(!$done)
-            <div>I'm not done</div>
+    <p>{{ trans_choice('messages.plural', 0, ['a' => 1]) }}</p>
+    <p>{{ trans_choice('messages.plural', 1, ['a' => 1]) }}</p>
+    <p>{{ trans_choice('messages.plural', 2, ['a' => 1]) }}</p>
 
-            @php
-                if (random_int(0, 1) === 1) $done = true
-            @endphp
-        @endwhile
-    </div>
+    <p>This is the content of the main page!</p>
 @endsection
