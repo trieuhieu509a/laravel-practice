6e66cc2cf82fbdaf731f942c71f535158318316b
Hieu Trinh <hieu.trinh@bliksund.com>
2022-11-06 17:00:44 +0700

    266. Testing API GET methods, verifying JSON structure II #210

--- tests/Feature/ApiPostCommentsTest.php
+++ tests/Feature/ApiPostCommentsTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use App\Models\Comment;
 use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -13,8 +14,8 @@ class ApiPostCommentsTest extends TestCase
 
     public function testNewBlogPostDoesNotHaveComments()
     {
-        BlogPost::factory(4)->create([
-            'user_id' => $this->user()->id,
+        BlogPost::factory()->create([
+            'user_id' => $this->user()->id
         ]);
 
         $response = $this->json('GET', 'api/v1/posts/1/comments');
@@ -22,5 +23,41 @@ class ApiPostCommentsTest extends TestCase
         $response->assertStatus(200)
             ->assertJsonStructure(['data', 'links', 'meta'])
             ->assertJsonCount(0, 'data');
+    }
+
+    public function testBlogPostHas10Comments()
+    {
+        BlogPost::factory()->create([
+            'user_id' => $this->user()->id
+        ])->each(function (BlogPost $post) {
+            $post->comments()->saveMany(
+                Comment::factory(10)->make([
+                    'user_id' => $this->user()->id
+                ])
+            );
+        });
+
+        $response = $this->json('GET', 'api/v1/posts/2/comments');
+
+        $response->assertStatus(200)
+            ->assertJsonStructure(
+                [
+                    'data' => [
+                        '*' => [
+                            'id',
+                            'contnt',
+                            'created_at',
+                            'updated_at',
+                            'user' => [
+                                'id',
+                                'name'
+                            ]
+                        ]
+                    ],
+                    'links',
+                    'meta'
+                ]
+            )
+            ->assertJsonCount(10, 'data');
     }
 }
